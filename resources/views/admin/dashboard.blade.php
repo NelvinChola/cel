a5c2b6f3b2c4e3d9aead2bd34c9415d58fb4dae0
Derick-chola <nelvinchola.dev@outlook.com>
2025-09-20 14:32:16 +0200

    removed the startic content from the admin dashboard. looking forward to include dynamic content

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,7 +13,7 @@
     </div>
 
     <!-- Content Row -->
-    <div class="row">
+    {{-- <div class="row">
 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
@@ -90,7 +90,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     <!-- Content Row -->
 
